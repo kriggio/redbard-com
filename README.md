5b75fcd6fc371abb1b7d9bdfde11c6b7accce423
Ken Riggio <kriggio@kennethr-mbr.telenav.com>
2021-02-06 11:53:37 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# redbard-com
---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const handlebars = require('express-handlebars');
+app.set('view engine', 'hbs');
+app.engine('hbs', handlebars({
+    layoutsDir: __dirname + '/views/layouts',
+    extname: 'hbs',
+    defaultLayout: 'planB',
+    partialsDir: __dirname + '/views/partials/'
+}));
+
+app.use(express.static('public'))
+
+
+app.get('/about', (req, res) => {
+    console.log('about');
+    res.render('about', {layout: 'index'});
+});
+
+app.get('/media', (req, res) => {
+    console.log('media');
+
+    res.render('media', {layout: 'index'});
+});
+
+app.get('/blog', (req, res) => {
+    console.log('blog');
+
+    res.render('blog', {layout: 'index'});
+});
+
+app.get('/projects', (req, res) => {
+    console.log('projects');
+
+    res.render('projects', {layout: 'index'});
+});
+
+app.get('/tos', (req, res) => {
+    console.log('tos');
+
+    res.render('tos', {layout: 'index'});
+});
+
+app.get('/privacypolicy', (req, res) => {
+    res.render('privacypolicy', {layout: 'index'});
+});
+
+app.get('/', (req, res) => {
+    res.render('main', {layout: 'index'});
+});
+
+
+app.listen(port, () => console.log(`App listening to port ${port}`));
+
